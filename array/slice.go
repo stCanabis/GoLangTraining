ccd22e4364bf8f1b38e275e65a43f82b11bba9b4
Laptev Andrei <rabote.laptev@mail.ru>
2020-07-26 14:15:31 +0700

    start learning GoLang

--- array/slice.go
+++ array/slice.go
@@ -2,16 +2,15 @@ package main
 
 import "fmt"
 
-func main()  {
-	slice := []int{1,2,3,4,5}
-	var data1 string =  "test string"
+func main() {
+	slice := []int{1, 2, 3, 4, 5}
+	var data1 string = "test string"
 	data2 := &data1
 	data3 := &slice[3]
-	fmt.Println("1",slice[3])
-	fmt.Println("2",data1)
-	fmt.Println("3",data2)
-	fmt.Println("4",*data2)
-	fmt.Println("5",data3)
-	fmt.Println("6",*data3)
-
+	fmt.Println("1", slice[3])
+	fmt.Println("2", data1)
+	fmt.Println("3", data2)
+	fmt.Println("4", *data2)
+	fmt.Println("5", data3)
+	fmt.Println("6", *data3)
 }
